9ba40538476650024d9f852efbbaa51d1076707b
GGBBB2000 <ggbbb@mineo.jp>
2020-03-25 14:57:10 +0900

    読み取りスクリプトをモジュール化した

---
+++ src/co2_monitor.rs
@@ -0,0 +1,79 @@
+extern crate hidapi;
+use hidapi::HidApi;
+
+const KEY: [u8; 8] = [0x86, 0x41, 0xc9, 0xa8, 0x7f, 0x41, 0x3c, 0xac];
+const OFFSET: [u32; 8] = [0x48, 0x74, 0x65, 0x6D, 0x70, 0x39, 0x39, 0x65];
+const SHUFFLE: [u32; 8] = [2, 4, 0, 7, 1, 6, 5, 3];
+
+pub struct Co2Monitor {
+    device: hidapi::HidDevice,
+}
+
+impl Co2Monitor {
+    pub fn new(vid: u16, pid: u16) -> Self {
+        let api = HidApi::new().expect("Co2-mini is not inserted!!");
+        Co2Monitor {
+            device: api.open(vid, pid).unwrap(),
+        }
+    }
+
+    pub fn init(&mut self) {
+        self.device.send_feature_report(&KEY).unwrap();
+    }
+
+    pub fn read(&self) -> Option<(u16, f32)> {
+        let mut buf = [0u8; 8];
+        let mut co2_checked = false;
+        let mut temp_checked = false;
+        let mut co2_val: u16 = 0;
+        let mut temp_val: f32 = 0f32;
+        while !(co2_checked && temp_checked) {
+            let res = self.device.read(&mut buf);
+            match res {
+                Ok(_) => {
+                    //println!("{:?}", &buf[..result]);
+                    let phase1 = SHUFFLE
+                        .iter()
+                        .map(|x| buf[*x as usize] as u32)
+                        .collect::<Vec<u32>>();
+                    //println!("{:?}", phase1);
+                    let phase2 = (0..=7)
+                        .map(|x| phase1[x] ^ KEY[x] as u32)
+                        .collect::<Vec<u32>>();
+                    //println!("{:?}", phase2);
+                    let phase3 = (0..=7)
+                        .map(|x| {
+                            (phase2[x] >> 3) | (phase2[((x as i16 + 7) % 8) as usize] << 5) & 0xff
+                        })
+                        .collect::<Vec<u32>>();
+                    //println!("{:?}", phase3);
+                    let ctmp = (0..=7)
+                        .map(|i| ((OFFSET[i] >> 4) | OFFSET[i] << 4) & 0xff)
+                        .collect::<Vec<u32>>();
+                    let result = (0..=7)
+                        .map(|i| ((0x100u32 + phase3[i] - ctmp[i]) & 0xff) as u8)
+                        .collect::<Vec<u8>>();
+                    //println!("{:?}", result);
+                    let val = ((result[1] as u16) << 8) | result[2] as u16;
+                    let op = result[0];
+                    match op {
+                        0x50 => {
+                            co2_checked = true;
+                            co2_val = val;
+                        }
+                        0x42 => {
+                            temp_checked = true;
+                            temp_val = val as f32 / 16.0 - 273.15;
+                        }
+                        _ => (),
+                    }
+                }
+                Err(_) => {
+                    println!("err");
+                    return None;
+                }
+            }
+        }
+        return Some((co2_val, temp_val));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,14 @@
 extern crate hidapi;
-use hidapi::HidApi;
-use std::thread::sleep;
-use std::time::Duration;
+mod co2_monitor;
+use co2_monitor::Co2Monitor;
 
 fn main() {
     let vendor_id = 0x04D9;
     let product_id = 0xA052;
-    let key = [0x86, 0x41, 0xc9, 0xa8, 0x7f, 0x41, 0x3c, 0xac];
-    let offset = [0x48,  0x74,  0x65,  0x6D,  0x70,  0x39,  0x39,  0x65];
-    let shuffle = [2, 4, 0, 7, 1, 6, 5, 3];
 
-    loop {
-        match HidApi::new() {
-            Ok(api) => {
-                let device = api.open(vendor_id, product_id).expect("CO2-mini is not inserted!!");
-                device.send_feature_report(&key).unwrap();
-                let mut buf = [0u8; 8];
-                let res = device.read(&mut buf);
-                match res {
-                    Ok(_) => {
-                        //println!("{:?}", &buf[..result]);
-                        let phase1 = shuffle.iter()
-                            .map(|x| buf[*x] as u32)
-                            .collect::<Vec<u32>>();
-                        //println!("{:?}", phase1);
-                        let phase2 = (0..=7).map(|x| phase1[x] ^ key[x] as u32)
-                            .collect::<Vec<u32>>();
-                        //println!("{:?}", phase2);
-                        let phase3 = (0..=7).map(|x| (phase2[x] >> 3) | (phase2[ ((x as i16 -1+8)%8) as usize ] << 5)  & 0xff)
-                            .collect::<Vec<u32>>();
-                        //println!("{:?}", phase3);
-                        let ctmp   = (0..=7).map(|i| ((offset[i] >> 4) | offset[i] << 4 )  & 0xff)
-                            .collect::<Vec<u32>>();
-                        let result = (0..7).map(|i| ((0x100u32 + phase3[i] - ctmp[i]) & 0xff) as u8 )
-                            .collect::<Vec<u8>>();
-                        //println!("{:?}", result);
-                        let val = ((result[1] as u16) << 8) | result[2] as u16;
-                        let op = result[0];
-                        match op {
-                            0x50 => println!("co2: {}", val),
-                            0x42 => println!("temp: {}", val as f32 / 16.0 - 273.15),
-                            _ => (),
-                        }
-
-                    },
-                    Err(_) => {
-                        println!("err");
-                    }
-                }
-            },
-            Err(e) => {
-                eprintln!("Error: {}", e);
-            },
-        }
-        let time = Duration::from_millis(1000);
-        sleep(time);
+    let mut co2_monitor = Co2Monitor::new(vendor_id, product_id);
+    co2_monitor.init();
+    if let Some((co2, temp)) = co2_monitor.read() {
+        println!("co2:{} temp:{:.1}", co2, temp);
     }
 }
