ed2de3dba0c76d5e09762dc2acf674edcf99ee05
GGBBB2000 <ggbbb@mineo.jp>
2020-03-26 14:34:19 +0900

    refactor: co2_monitor::read()
    エラー処理を追加

--- src/co2_monitor.rs
+++ src/co2_monitor.rs
@@ -6,15 +6,15 @@ pub struct Co2Monitor {
 
 impl Co2Monitor {
     const KEY: [u8; 8] = [0x86, 0x41, 0xc9, 0xa8, 0x7f, 0x41, 0x3c, 0xac];
-    const OFFSET: [u32; 8] = [0x48, 0x74, 0x65, 0x6D, 0x70, 0x39, 0x39, 0x65];
+    const OFFSET: [u32; 8] = [0x48, 0x74, 0x65, 0x6D, 0x70, 0x39, 0x39, 0x65]; // "Htemp99e"
     const SHUFFLE: [u32; 8] = [2, 4, 0, 7, 1, 6, 5, 3];
     const VID: u16 = 0x04D9;
     const PID: u16 = 0xA052;
 
     pub fn new() -> Self {
-        let api = HidApi::new().expect("Co2-mini is not inserted!!");
+        let api = HidApi::new().unwrap();
         Co2Monitor {
-            device: api.open(Self::VID, Self::PID).unwrap(),
+            device: api.open(Self::VID, Self::PID).expect("CO2-mini is not detected!! or Permission Denied!!"),
         }
     }
 
@@ -22,59 +22,56 @@ impl Co2Monitor {
         self.device.send_feature_report(&Self::KEY).unwrap();
     }
 
-    pub fn read(&self) -> Option<(u16, f32)> {
+    pub fn read(&self) -> Result<(u16, f32), hidapi::HidError> {
         let mut buf = [0u8; 8];
         let mut co2_checked = false;
         let mut temp_checked = false;
         let mut co2_val: u16 = 0;
         let mut temp_val: f32 = 0f32;
         while !(co2_checked && temp_checked) {
-            let res = self.device.read(&mut buf);
-            match res {
-                Ok(_) => {
-                    //println!("{:?}", &buf[..result]);
-                    let phase1 = &Self::SHUFFLE
-                        .iter()
-                        .map(|x| buf[*x as usize] as u32)
-                        .collect::<Vec<u32>>();
-                    //println!("{:?}", phase1);
-                    let phase2 = (0..=7)
-                        .map(|x| phase1[x] ^ Self::KEY[x] as u32)
-                        .collect::<Vec<u32>>();
-                    //println!("{:?}", phase2);
-                    let phase3 = (0..=7)
-                        .map(|x| {
-                            (phase2[x] >> 3) | (phase2[((x as i16 + 7) % 8) as usize] << 5) & 0xff
-                        })
-                        .collect::<Vec<u32>>();
-                    //println!("{:?}", phase3);
-                    let ctmp = (0..=7)
-                        .map(|i| ((Self::OFFSET[i] >> 4) | Self::OFFSET[i] << 4) & 0xff)
-                        .collect::<Vec<u32>>();
-                    let result = (0..=7)
-                        .map(|i| ((0x100u32 + phase3[i] - ctmp[i]) & 0xff) as u8)
-                        .collect::<Vec<u8>>();
-                    //println!("{:?}", result);
-                    let val = ((result[1] as u16) << 8) | result[2] as u16;
-                    let op = result[0];
-                    match op {
-                        0x50 => {
-                            co2_checked = true;
-                            co2_val = val;
-                        }
-                        0x42 => {
-                            temp_checked = true;
-                            temp_val = val as f32 / 16.0 - 273.15;
-                        }
-                        _ => (),
-                    }
+            self.device.read(&mut buf)?;
+            let result = Self::decrypt(buf);
+            let val = ((result[1] as u16) << 8) | result[2] as u16;
+            let op = result[0];
+            match op {
+                0x50 => {
+                    co2_checked = true;
+                    co2_val = val;
                 }
-                Err(_) => {
-                    println!("err");
-                    return None;
+                0x42 => {
+                    temp_checked = true;
+                    temp_val = val as f32 / 16.0 - 273.15;
                 }
+                _ => (),
             }
         }
-        Some((co2_val, temp_val))
+        Ok((co2_val, temp_val))
+    }
+
+    fn decrypt(buf: [u8; 8]) -> Vec<u8> {
+        let phase1 = &Self::SHUFFLE
+            .iter()
+            .map(|&x| buf[x as usize] as u32)
+            .collect::<Vec<_>>();
+        //println!("{:?}", phase1);
+        let phase2 = phase1
+            .iter()
+            .zip(Self::KEY.iter())
+            .map(|(&p1, &k)| p1 ^ k as u32)
+            .collect::<Vec<_>>();
+        //println!("{:?}", phase2);
+        let phase3 = (0..=7)
+            .map(|x| (phase2[x] >> 3) | (phase2[(x + 7) % 8] << 5) & 0xff)
+            .collect::<Vec<_>>();
+        //println!("{:?}", phase3);
+        let ctmp = Self::OFFSET
+            .iter()
+            .map(|o| ((o >> 4) | o << 4) & 0xff)
+            .collect::<Vec<_>>();
+        phase3
+            .into_iter()
+            .zip(ctmp.into_iter())
+            .map(|(p3, c)| ((0x100u32 + p3 - c) & 0xff) as u8)
+            .collect::<Vec<_>>()
     }
 }
